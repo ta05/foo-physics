55274a10a73f5944d0e6311b6f7784c163f155b8
Tolu Alimi <talimi2000@gmail.com>
2024-05-30 17:37:28 -0500

    Added unit tests for volume

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,1 +1,13 @@
-from foo import foo
+from foo.foo import volume
+
+def test_volume():
+    """Test volume function"""
+    expected = {
+        0: 0.0,
+        10: 4188.790204786391,
+        198: 32515031.57531224,
+        7.11: 1505.5577247239269,
+    }
+    for k in expected:
+        actual = volume(k)
+        assert actual == expected[k], f"Incorrect, the volume of a sphere of radius {k} is {expected[k]} not {actual}"
